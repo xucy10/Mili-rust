8de0ae81ea30d4b22762d274666aee33ad6b08dc
xucy <someonm201141923@outlook.com>
2026-07-11 22:04:06 +0800

    fix: 更新 Clippy 允许列表以包含更多 lint 规则

--- crates/valence_world/src/lib.rs
+++ crates/valence_world/src/lib.rs
@@ -1,4 +1,12 @@
-#![allow(clippy::absurd_extreme_comparisons)]
+#![allow(
+    clippy::doc_markdown,
+    clippy::impl_trait_in_params,
+    clippy::cast_lossless,
+    clippy::uninlined_format_args,
+    clippy::map_unwrap_or,
+    clippy::field_reassign_with_default,
+    clippy::ref_patterns
+)]
 
 pub mod level_dat;
 pub mod save_system;
