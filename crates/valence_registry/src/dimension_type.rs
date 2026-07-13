68f47ab45652ea6dc3897de088a5f7fd022d476f
xucy10 <someonm201141923@outlook.com>
2026-07-13 20:07:56 +0800

    fix: MonsterSpawnLightLevel serde tag format

--- crates/valence_registry/src/dimension_type.rs
+++ crates/valence_registry/src/dimension_type.rs
@@ -232,7 +232,7 @@ pub enum MonsterSpawnLightLevel {
 }
 
 #[derive(Copy, Clone, PartialEq, Debug, Serialize, Deserialize)]
-#[serde(tag = "type", content = "value")]
+#[serde(tag = "type")]
 pub enum MonsterSpawnLightLevelTagged {
     #[serde(rename = "minecraft:uniform")]
     Uniform {
